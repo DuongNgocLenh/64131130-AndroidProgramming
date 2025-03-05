572ea582c6e7937fc7fbc0ad000657b45f7b29ee
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-05 18:29:57 +0700

    Tạo Ex4 và làm

--- Ex4_AddSubMulDiv_Onclick/app/src/main/java/ntu/duongngoclenh/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_Onclick/app/src/main/java/ntu/duongngoclenh/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -1,83 +1,83 @@
-package ntu.duongngoclenh.ex4_addsubmuldiv_onclick;
+    package ntu.duongngoclenh.ex4_addsubmuldiv_onclick;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    import androidx.activity.EdgeToEdge;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.graphics.Insets;
+    import androidx.core.view.ViewCompat;
+    import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    //Khai bao cac đối tượng gắn với đk tương ứng ở đây
-    EditText so1;
-    EditText so2;
-    EditText editTextKQ;
-    Button nutCong,nutTru,nutNhan,nutChia;
-    void TimDieuKhien(){
-        so1 = (EditText)findViewById(R.id.edtSO1);
-        so2 = (EditText)findViewById(R.id.edtSO2);
-        editTextKQ = (EditText)findViewById(R.id.edtKetQua);
-        nutCong = (Button)findViewById(R.id.btnCong);
-        nutTru = (Button)findViewById(R.id.btnTru);
-        nutNhan = (Button)findViewById(R.id.btnNhan);
-        nutChia = (Button)findViewById(R.id.btnChia);
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        TimDieuKhien();
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+    public class MainActivity extends AppCompatActivity {
+        //Khai bao cac đối tượng gắn với đk tương ứng ở đây
+        EditText so1;
+        EditText so2;
+        EditText editTextKQ;
+        Button nutCong,nutTru,nutNhan,nutChia;
+        void TimDieuKhien(){
+            so1 = (EditText)findViewById(R.id.edtSO1);
+            so2 = (EditText)findViewById(R.id.edtSO2);
+            editTextKQ = (EditText)findViewById(R.id.edtKetQua);
+            nutCong = (Button)findViewById(R.id.btnCong);
+            nutTru = (Button)findViewById(R.id.btnTru);
+            nutNhan = (Button)findViewById(R.id.btnNhan);
+            nutChia = (Button)findViewById(R.id.btnChia);
+        }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_main);
+            TimDieuKhien();
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
 
-    //Xu Ly Cong
-    public void XuLyCong(View v){
-        //xu ly code
-        String sothu1 = so1.getText().toString();
-        String sothu2 = so2.getText().toString();
-        float soA = Float.parseFloat(sothu1);
-        float soB = Float.parseFloat(sothu2);
-        float Tong = soA + soB;
-        String chuoiKQ = String.valueOf(Tong);
-        editTextKQ.setText(chuoiKQ);
+        //Xu Ly Cong
+        public void XuLyCong(View v){
+            //xu ly code
+            String sothu1 = so1.getText().toString();
+            String sothu2 = so2.getText().toString();
+            float soA = Float.parseFloat(sothu1);
+            float soB = Float.parseFloat(sothu2);
+            float Tong = soA + soB;
+            String chuoiKQ = String.valueOf(Tong);
+            editTextKQ.setText(chuoiKQ);
+        }
+        public void XuLyTru(View v){
+            //xu ly code
+            String sothu1 = so1.getText().toString();
+            String sothu2 = so2.getText().toString();
+            float soA = Float.parseFloat(sothu1);
+            float soB = Float.parseFloat(sothu2);
+            float Tong = soA - soB;
+            String chuoiKQ = String.valueOf(Tong);
+            editTextKQ.setText(chuoiKQ);
+        }
+        public void XuLyNhan(View v){
+            //xu ly code
+            String sothu1 = so1.getText().toString();
+            String sothu2 = so2.getText().toString();
+            float soA = Float.parseFloat(sothu1);
+            float soB = Float.parseFloat(sothu2);
+            float Tong = soA * soB;
+            String chuoiKQ = String.valueOf(Tong);
+            editTextKQ.setText(chuoiKQ);
+        }
+        public void XuLyChia(View v){
+            //xu ly code
+            String sothu1 = so1.getText().toString();
+            String sothu2 = so2.getText().toString();
+            float soA = Float.parseFloat(sothu1);
+            float soB = Float.parseFloat(sothu2);
+            float Tong = soA / soB;
+            String chuoiKQ = String.valueOf(Tong);
+            editTextKQ.setText(chuoiKQ);
+        }
     }
-    public void XuLyTru(View v){
-        //xu ly code
-        String sothu1 = so1.getText().toString();
-        String sothu2 = so2.getText().toString();
-        float soA = Float.parseFloat(sothu1);
-        float soB = Float.parseFloat(sothu2);
-        float Tong = soA - soB;
-        String chuoiKQ = String.valueOf(Tong);
-        editTextKQ.setText(chuoiKQ);
-    }
-    public void XuLyNhan(View v){
-        //xu ly code
-        String sothu1 = so1.getText().toString();
-        String sothu2 = so2.getText().toString();
-        float soA = Float.parseFloat(sothu1);
-        float soB = Float.parseFloat(sothu2);
-        float Tong = soA * soB;
-        String chuoiKQ = String.valueOf(Tong);
-        editTextKQ.setText(chuoiKQ);
-    }
-    public void XuLyChia(View v){
-        //xu ly code
-        String sothu1 = so1.getText().toString();
-        String sothu2 = so2.getText().toString();
-        float soA = Float.parseFloat(sothu1);
-        float soB = Float.parseFloat(sothu2);
-        float Tong = soA / soB;
-        String chuoiKQ = String.valueOf(Tong);
-        editTextKQ.setText(chuoiKQ);
-    }
-}
---
+++ Ex5_AddSubMulDiv_Anynomous/app/src/main/java/ntu/duongngoclenh/ex5_addsubmuldiv_anynomous/MainActivity.java
@@ -0,0 +1,93 @@
+package ntu.duongngoclenh.ex5_addsubmuldiv_anynomous;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    // Khai báo các đối tượng gắn với điều khiển tương ứng
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong, nutTru, nutNhan, nutChia;
+
+    void TimDieuKhien() {
+        editTextSo1 = findViewById(R.id.edtSo1);
+        editTextSo2 = findViewById(R.id.edtSo2);
+        editTextKQ = findViewById(R.id.edtKetQua);
+        nutCong = findViewById(R.id.btnCong);
+        nutTru = findViewById(R.id.btnTru);
+        nutNhan = findViewById(R.id.btnNhan);
+        nutChia = findViewById(R.id.btnChia);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TimDieuKhien();
+
+        // Gắn bộ lắng nghe sự kiện cho từng nút
+        nutCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XULY_CONG();
+            }
+        });
+
+        nutTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XULY_TRU();
+            }
+        });
+
+        nutNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XULY_NHAN();
+            }
+        });
+
+        nutChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XULY_CHIA();
+            }
+        });
+    }
+
+    void XULY_CONG() {
+        float num1 = Float.parseFloat(editTextSo1.getText().toString());
+        float num2 = Float.parseFloat(editTextSo2.getText().toString());
+        float tong = num1 + num2;
+        editTextKQ.setText(String.valueOf(tong));
+    }
+
+    void XULY_TRU() {
+        float num1 = Float.parseFloat(editTextSo1.getText().toString());
+        float num2 = Float.parseFloat(editTextSo2.getText().toString());
+        float hieu = num1 - num2;
+        editTextKQ.setText(String.valueOf(hieu));
+    }
+
+    void XULY_NHAN() {
+        float num1 = Float.parseFloat(editTextSo1.getText().toString());
+        float num2 = Float.parseFloat(editTextSo2.getText().toString());
+        float tich = num1 * num2;
+        editTextKQ.setText(String.valueOf(tich));
+    }
+
+    void XULY_CHIA() {
+        float num1 = Float.parseFloat(editTextSo1.getText().toString());
+        float num2 = Float.parseFloat(editTextSo2.getText().toString());
+        if (num2 != 0) {
+            float thuong = num1 / num2;
+            editTextKQ.setText(String.valueOf(thuong));
+        } else {
+            editTextKQ.setText("Lỗi: Chia cho 0");
+        }
+    }
+}
