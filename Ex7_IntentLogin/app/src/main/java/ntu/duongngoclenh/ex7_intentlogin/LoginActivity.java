b5c91e0f77edb1806b2d4ddf33abf552a617c013
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-06 15:35:12 +0700

    Xu ly xong dang nhap va lam ham xu ly login

--- Ex7_IntentLogin/app/src/main/java/ntu/duongngoclenh/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/duongngoclenh/ex7_intentlogin/LoginActivity.java
@@ -1,6 +1,9 @@
 package ntu.duongngoclenh.ex7_intentlogin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,17 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnXacNhan = (Button) findViewById(R.id.btnOK);
+
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xu ly
+                EditText edTenDN = (EditText) findViewById(R.id.edtUsername);
+                EditText edPass = (EditText) findViewById(R.id.edtPass);
+            }
         });
+        }
     }
-}
--- Ex7_IntentLogin/app/src/main/java/ntu/duongngoclenh/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/duongngoclenh/ex7_intentlogin/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.duongngoclenh.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         //Tim dk
         Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
         //Su kien
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Lenh xu ly o day
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
         });
     }
 }
