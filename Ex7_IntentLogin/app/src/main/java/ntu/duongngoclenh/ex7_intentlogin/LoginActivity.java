4ba342b3d1db4c43d9e1af957891ac8dd0519d95
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-06 15:47:17 +0700

    xu ly dang nhap va bi loi dung ung dung

--- Ex7_IntentLogin/app/src/main/java/ntu/duongngoclenh/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/duongngoclenh/ex7_intentlogin/LoginActivity.java
@@ -1,9 +1,11 @@
 package ntu.duongngoclenh.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,10 +24,31 @@ public class LoginActivity extends AppCompatActivity {
 
         btnXacNhan.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //Xu ly
+            public void onClick(View view) {
+                //Xu ly dang nhap
+                //Lay du lieu
+                //B1 tim tham chieu den du lieu
                 EditText edTenDN = (EditText) findViewById(R.id.edtUsername);
                 EditText edPass = (EditText) findViewById(R.id.edtPass);
+                //B2 lay du lieu
+                String tenDangNhap = edTenDN.getText().toString();
+                String mk = edPass.getText().toString();
+
+                //Kiem tra mk
+                if( tenDangNhap.equals("duongngoclenh") && mk.equals("123") )//mk dung
+                {
+                    //chuyen sang man hinh home
+                    Intent iQuiz = new Intent(LoginActivity.this, HomeActivity.class);
+                    //Goi du lieu, dang key-value, key dung de dung de ben kia loc du lieu
+                    iQuiz.putExtra("ten_dang_nhap",tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap",mk);
+                    //Gui di
+                    startActivity(iQuiz);
+
+                }else
+                {
+                    Toast.makeText(LoginActivity.this,"BẠN NHẬP SAI THÔNG TIN",Toast.LENGTH_LONG);
+                }
             }
         });
         }
