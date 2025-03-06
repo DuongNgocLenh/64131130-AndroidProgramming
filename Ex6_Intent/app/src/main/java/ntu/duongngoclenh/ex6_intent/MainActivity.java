ae361df555ad3dd5a1b25dee816ebe72ed03f232
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-06 14:31:26 +0700

    Tao va xu ly bo lang nghe tren 2 man hinh

--- Ex6_Intent/app/src/main/java/ntu/duongngoclenh/ex6_intent/MainActivity.java
+++ Ex6_Intent/app/src/main/java/ntu/duongngoclenh/ex6_intent/MainActivity.java
@@ -1,5 +1,6 @@
 package ntu.duongngoclenh.ex6_intent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,21 +21,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         //Tìm điều khiển nút bấm
         TimDieuKhien();
         // Gắn bộ lắng nghe sự kiện
         nutMH2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //
+            public void onClick(View view) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo intent 2 tham số (MH hiện tại.this> MH2.class)
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                //B2 gửi:
+                startActivity(intentMH2);
             }
         });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo intent 2 tham số (MH hiện tại.this> MH3.class)
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                //B2 gửi:
+                startActivity(intentMH3);
+            }
         });
     }
 
