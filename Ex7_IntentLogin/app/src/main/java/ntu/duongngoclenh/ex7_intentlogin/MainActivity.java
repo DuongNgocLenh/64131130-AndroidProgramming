11e1fd0007a591c13d36ba02b505cebe25755a19
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-06 15:18:06 +0700

    lam xong cac quiz va xu ly ham ben main java

--- Ex7_IntentLogin/app/src/main/java/ntu/duongngoclenh/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/duongngoclenh/ex7_intentlogin/MainActivity.java
@@ -1,6 +1,7 @@
 package ntu.duongngoclenh.ex7_intentlogin;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //Tim dk
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        //Su kien
         });
     }
 }
