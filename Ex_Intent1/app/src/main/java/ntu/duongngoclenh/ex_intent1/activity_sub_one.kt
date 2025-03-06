d1b7480e56a48bdfce5bcea59a3931f33b905644
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-06 09:07:12 +0700

    Tao ex intent chuyển màn hình

---
+++ Ex_Intent1/app/src/main/java/ntu/duongngoclenh/ex_intent1/activity_sub_one.kt
@@ -0,0 +1,47 @@
+package ntu.duongngoclenh.ex_intent1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ntu.duongngoclenh.ex_intent1.ui.theme.Ex_Intent1Theme
+
+class activity_sub_one : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ex_Intent1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ex_Intent1Theme {
+        Greeting("Android")
+    }
+}
