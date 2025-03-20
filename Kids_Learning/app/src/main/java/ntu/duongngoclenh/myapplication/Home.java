dbf645e205c9a81c8ff3fc169be8d7e1a86920cc
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-20 19:47:59 +0700

    Tao mh2 home

--- Kids_Learning/app/src/main/java/ntu/duongngoclenh/myapplication/Home.java
+++ Kids_Learning/app/src/main/java/ntu/duongngoclenh/myapplication/Home.java
@@ -1,9 +1,6 @@
 package ntu.duongngoclenh.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button home = findViewById(R.id.btnhome);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mhhome = new Intent(MainActivity.this,Home.class);
-                startActivity(mhhome);
-            }
         });
     }
 }
