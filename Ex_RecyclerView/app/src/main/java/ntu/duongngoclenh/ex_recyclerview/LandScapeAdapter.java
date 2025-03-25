e0a8f2419410f6568e01d1d1067d84d1554b1cf3
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-25 20:37:27 +0700

    xu ly lay thong tin anh ve text, xu ly itemcount

--- Ex_RecyclerView/app/src/main/java/ntu/duongngoclenh/ex_recyclerview/LandScapeAdapter.java
+++ Ex_RecyclerView/app/src/main/java/ntu/duongngoclenh/ex_recyclerview/LandScapeAdapter.java
@@ -33,12 +33,21 @@ public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.Item
     @Override
     public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
         //Lay doi tuong hien thi
+        LandScape landScape = lsdata.get(position);
+        //Trich thong tin
+        String caption = landScape.getCation();
+        String tenanh = landScape.getImage();
+        //Dat vao cac truong thong tin cua holder
+        holder.tvCaption.setText(caption);
+        String packagename = holder.itemView.getContext().getPackageName();
+        int imageID = holder.itemView.getResources().getIdentifier(tenanh,"mipmap",packagename);
+        holder.imageViewLandscape.setImageResource(imageID);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lsdata.size();
     }
 
     class ItemLandHolder extends RecyclerView.ViewHolder{
