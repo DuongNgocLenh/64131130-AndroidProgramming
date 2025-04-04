583a37e3e7d0b45dff4bfd4fce48142f6001a995
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-04-04 16:01:29 +0700

    tao cac nut de chuan bi xu ly su kien intent

--- ThiGK2.DuongNgocLenh/app/src/main/java/ntu/duongngoclenh/thigk2duongngoclenh/MainActivity.java
+++ ThiGK2.DuongNgocLenh/app/src/main/java/ntu/duongngoclenh/thigk2duongngoclenh/MainActivity.java
@@ -1,6 +1,7 @@
 package ntu.duongngoclenh.thigk2duongngoclenh;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button cn2 = findViewById(R.id.btn2);
+        Button cn3 = findViewById(R.id.btn3);
+        Button cn4 = findViewById(R.id.btn4);
+        Button me = findViewById(R.id.btnMe);
     }
 }
